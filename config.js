4e36f240590e5c792e3968257a6dee77e272231c
Hiếu <60700486+PhanXuanHieu@users.noreply.github.com>
2021-09-04 20:59:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,33 @@
+
+var config = {
+
+    texts: [
+        "Tuổi mới luôn vui vẻ",
+        "Cuộc sống tràn ngập niềm vui",
+        "Ngày càng xinh đẹp nữa",
+        "Học giỏi hơn nữa này",
+        "Thành công trong cuộc sống",
+        "Hạnh phúc bên gia đình",
+        "Chúc bạn sớm có người yêu",
+        "Có thể không ai nhớ ngày của bạn",
+        "Nhưng mình vẫn nhớ nha:v",
+        "Mình muốn nói là",
+        "CHÚC BẠN LUÔN MAY MẮN VÀ THÀNH CÔNG TRÊN CON ĐƯỜNG ĐÃ CHỌN",
+        "HAPPY BIRTHDAY TO YOU~~",
+    ],
+
+    imgs: {
+        "Ngày càng xinh đẹp nữa": "./imgs/xiaokeai.png",
+        "Chúc bạn sớm có người yêu": "./imgs/ny.jpg",
+    },
+    desc: {
+        turn_on: "Bắt đầu nào",
+        play: "Bật tý nhạc lên chứ",
+        bannar_coming: "Mang poster ra nào",
+        balloons_flying: "Thêm một tý bóng bay",
+        cake_fadein: "Mang bánh lên nữa chứ",
+        light_candle: "Thắp nến lên nào",
+        wish_message: "HAPPY BIRTHDAY TO YOU",
+        story: "Có những lời chúc cho bạn nè",
+    }
+};
---
+++ js/sccript.js
@@ -0,0 +1,6 @@
+function clickMe() {
+  document.getElementById("img").style.display = "block";
+  document.getElementById("content").style.display = "block";
+  document.getElementById("button").style.display = "none";
+  document.getElementById("link").style.display = "block";
+}
